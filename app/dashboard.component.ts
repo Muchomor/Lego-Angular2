a529b339e743069719d12472f1a09b9e53c93ebd
Szewcow Tomasz <tomasz.szewcow.ext@adcubum.com>
2017-03-07 15:23:19 +0100

    TODOS for the first excercise refined

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -1,4 +1,4 @@
-// TODO: import @Component decorator from @angular/core and use it to create DashbaordComponent
-// TODO: create items property
+// TODO: import @Component decorator from @angular/core and use it to create DashboardComponent
+// TODO: create items array property as placeholder for legoSets
 // TODO: import data from data.ts file and assign it to the items property
 // TODO: create template for the component and list the data (use *ngFor directive) 
