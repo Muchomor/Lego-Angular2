c3f6d2ed7e2d7ff3e3f14e057849c9bb56584c24
Szewcow Tomasz <tomasz.szewcow.ext@adcubum.com>
2017-03-07 16:51:41 +0100

    todo adjusted

---
+++ app/legoShop.service.ts
@@ -0,0 +1,16 @@
+import { LegoShopSet } from './LegoShopSet';
+// TODO: import @Injectable decorator from @angular/core
+// TODO: use the decorator to add LegoShopService
+// TODO: implement methods, return data from data.ts file
+export class LegoShopService {
+
+    getLegoSets(): LegoShopSet[] {
+        // TODO: to be implemented
+        throw new Error('not implemented');
+    }
+
+    getTop3Sets(): LegoShopSet[] {
+        // TODO: to be implemented
+        throw new Error('not implemented');
+    }
+}
