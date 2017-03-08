1ffd8d2b5a4da57b9f075f06bc4ce2969232921b
Szewcow Tomasz <tomasz.szewcow.ext@adcubum.com>
2017-03-08 12:37:10 +0100

    routing refactored to app routing module

---
+++ app/app-routing.module.ts
@@ -0,0 +1,46 @@
+import { LegoSetDetailsComponent } from './lego/lego-set-details/legoSetDetails.component';
+import { LegoSetsComponent } from './lego/lego-sets/legoSets.component';
+import { DashboardComponent } from './lego-shop/dashboard/dashboard.component';
+import { RouterModule, Route } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { LegoShopSetsComponent } from './lego-shop/lego-shop-sets/legoShopSets.component';
+import { LegoShopDetailsComponent } from './lego-shop/lego-shop-details/legoShopDetails.component';
+
+
+const appRoutes: Route[] = [
+    {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+    },
+    {
+        path: 'dashboard',
+        component: DashboardComponent
+    },
+    {
+        path: 'lego-sets',
+        component: LegoSetsComponent
+    },
+    {
+        path: 'lego-set-details',
+        component: LegoSetDetailsComponent
+    },
+    {
+        path: 'lego-set-details/:legoSetId',
+        component: LegoSetDetailsComponent
+    },
+    {
+        path: 'lego-shop',
+        component: LegoShopSetsComponent
+    },
+    {
+        path: 'lego-shop-details/:legoShopSetId',
+        component: LegoShopDetailsComponent
+    }
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(appRoutes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule {}
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,56 +1,17 @@
-import { LegoModule } from './lego/lego.module';
-import { LegoSetDetailsComponent } from './lego/lego-set-details/legoSetDetails.component';
-import { LegoSetsComponent } from './lego/lego-sets/legoSets.component';
-import { LegoShopModule } from './lego-shop/legoShop.module';
-import { FormsModule } from '@angular/forms';
-import { LegoShopDetailsComponent } from './lego-shop/lego-shop-details/legoShopDetails.component';
-import { LegoShopSetsComponent } from './lego-shop/lego-shop-sets/legoShopSets.component';
-import { RouterModule, Routes } from '@angular/router';
 import { LegoShopService } from './shared/legoShop.service';
-import { DashboardComponent } from './lego-shop/dashboard/dashboard.component';
+import { AppRoutingModule } from './app-routing.module';
+import { LegoModule } from './lego/lego.module';
+import { LegoShopModule } from './lego-shop/legoShop.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-
-const appRoutes: Routes = [
-    {
-        path: '',
-        redirectTo: '/dashboard',
-        pathMatch: 'full'
-    },
-    {
-        path: 'dashboard',
-        component: DashboardComponent
-    },
-    {
-        path: 'lego-sets',
-        component: LegoSetsComponent
-    },
-    {
-        path: 'lego-set-details',
-        component: LegoSetDetailsComponent
-    },
-    {
-        path: 'lego-set-details/:legoSetId',
-        component: LegoSetDetailsComponent
-    },
-    {
-        path: 'lego-shop',
-        component: LegoShopSetsComponent
-    },
-    {
-        path: 'lego-shop-details/:legoShopSetId',
-        component: LegoShopDetailsComponent
-    }
-];
-
-// TODO: create AppRoutingModule and move routes definition there, import AppRoutingModule in AppModule
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-    imports: [BrowserModule, RouterModule.forRoot(appRoutes), FormsModule, LegoShopModule, LegoModule],
-    declarations: [AppComponent],
-    bootstrap: [AppComponent],
-    providers: [LegoShopService]
+  imports: [BrowserModule, LegoModule, LegoShopModule, AppRoutingModule, FormsModule],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+  providers: [LegoShopService]
 })
 export class AppModule {
 }
