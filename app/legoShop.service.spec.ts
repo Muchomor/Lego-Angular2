8452936edfda25c5aff1961635284cfc47a0ed2f
Szewcow Tomasz <tomasz.szewcow.ext@adcubum.com>
2017-03-08 08:18:31 +0100

    testing exercise todos added

---
+++ app/legoShop.service.spec.ts
@@ -0,0 +1,26 @@
+// TODO: import LegoShopService
+
+describe('LegoShopService Tests', () => {
+
+    // TODO: create variable for LegoShopService
+
+    beforeEach(() => {
+        // TODO: instantiate LegoShopService and assign in to created variable 
+    });
+
+    it('should return all lego sets', () => {
+        // TODO: first implement test
+    });
+
+    it('should return filtered lego sets', () => {
+        // TODO: first implement test
+    });
+
+    it('should return top 3 lego sets', () => {
+        // TODO: first implement test
+    });
+
+    it('should find one lego set', () => {
+        // TODO: first implement test
+    });
+});
--- app/legoShop.service.ts
+++ app/legoShop.service.ts
@@ -7,6 +7,8 @@ export class LegoShopService {
 
     private legoSets: LegoShopSet[] = data;
 
+    // TODO: modify this method by adding optional query string parameter and filter the results
+    // Hint: getLegoSets(query?: string): LegoShopSet[]
     getLegoSets(): LegoShopSet[] {
         return this.legoSets;
     }
@@ -14,4 +16,9 @@ export class LegoShopService {
     getTop3Sets(): LegoShopSet[] {
         return this.legoSets.splice(0, 3);
     }
+
+    findOne(id: string): LegoShopSet[] {
+        // TODO: implement method
+        throw (new Error('not implemented yet'));
+    };
 }
