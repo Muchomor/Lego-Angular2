6743669409ee3261da9411bcebdac67367d936fd
Szewcow Tomasz <tomasz.szewcow.ext@adcubum.com>
2017-03-08 10:54:08 +0100

    intern todos added for routing part

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,7 +4,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
+// TODO: create app paths (array of Routes type -> import Routes type from @angular/router)
+// TODO: set DashboardComponent as default dialog
+// TODO: create LegoShopDetailsComponent, create route for the dialog and load selected LegoShopSet on dialog startup
+// TODO: create LegoShopSetsComponent, create route for the dialog and load all LegoShopSets on dialog startup
+// TODO: move DashboardComponent, LegoShopDetailsComponent, LegoShopSetsComponent to the app/lego-shop directory
+// TODO: create LegoShopModule in app/lego-shop directory
+// TODO: move DashboardComponent, LegoShopDetailsComponent, LegoShopSetsComponent to the LegoShopModule
+// TODO: import LegoShopModule into the AppModule
+// TODO: create LegoModule in app/lego directory
+// TODO: create LegoSetDetailsComponent in the app/lego directory (LegoModule), create route for the dialog
+// TODO: create LegoSetsComponent in the app/lego directory (LegoModule), create route for the dialog
+// TODO: import LegoModule into the AppModule
+// TODO: create AppRoutingModule and move routes definition there, import AppRoutingModule in AppModule
+
 @NgModule({
+  // TODO: import RouterModule from @angular/router and call forRoot static method with app paths
+  // TODO: import FormsModule from @angular/forms
   imports: [BrowserModule],
   declarations: [AppComponent, DashboardComponent],
   bootstrap: [AppComponent],
