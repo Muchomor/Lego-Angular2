392aacead2101a47ed2c3f2ffe5e830fd29672e9
Bartlomiej Pietrowski <bartlomiej.pietrowski@capgemini.com>
2017-03-09 00:01:01 +0100

    Added forms module and template reference variables

--- app/lego/lego.module.ts
+++ app/lego/lego.module.ts
@@ -4,9 +4,10 @@ import { LegoSetService } from './legoSet.service';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-    imports: [CommonModule, RouterModule],
+    imports: [CommonModule, RouterModule, FormsModule],
     declarations: [LegoSetsComponent, LegoSetDetailsComponent],
     exports: [],
     providers: [LegoSetService]
